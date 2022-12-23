53239de076a78b83a0ae56d3ce34e15bba56e117
Esakiraj21 <esakiraj19@gmail.com>
2022-12-23 20:15:21 +0530

    add 2 numbers

---
+++ add two integer numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int a, b,add;
+string c,d;
+Console.WriteLine("enter First integer no");
+a=Convert.ToInt32((d=Console.ReadLine())==""?"0":d);
+Console.WriteLine("enter Second integer no");
+b =Convert.ToInt32((c=Console.ReadLine())==""?"0":c);
+add = a + b;
+Console.WriteLine($"addition of two number is {add}");
+Console.ReadLine();
